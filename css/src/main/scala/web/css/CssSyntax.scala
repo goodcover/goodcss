9377c5d306d5994ff91b585854b223e585a80b08
Jeremy Hughes <jedahu@gmail.com>
2022-08-26 14:38:47 +1200

    CSS: goodbye my sweet ?-

--- css/src/main/scala/web/css/CssSyntax.scala
+++ css/src/main/scala/web/css/CssSyntax.scala
@@ -13,30 +13,30 @@ private[css] object CssSyntax {
 
     def :-(x0: CssValue, x1: CssValue, xs: CssValue*): CssRule = CssRule(name, CssDelimited(x0 +: x1 +: xs))
 
-    def ?-[A](rhs: Option[A])(implicit ev: A => CssRhs[CssValue]): CssRule =
+    def :-[A](rhs: Option[A])(implicit ev: A => CssRhs[CssValue]): CssRule =
       CssRule(name, rhs.fold(CssRhs.empty)(ev))
 
-    def ?-[A](rhs: js.UndefOr[A])(implicit ev: A => CssRhs[CssValue]): CssRule =
+    def :-[A](rhs: js.UndefOr[A])(implicit ev: A => CssRhs[CssValue]): CssRule =
       CssRule(name, rhs.fold(CssRhs.empty)(ev))
   }
 
   final class CssRuleExprSyntax private[css] (val name: String) extends AnyVal {
     def :-(rhs: CssRhs[CssExpr]): CssRule = CssRule(name, rhs)
 
-    def ?-[A](rhs: Option[A])(implicit ev: A => CssRhs[CssExpr]): CssRule =
+    def :-[A](rhs: Option[A])(implicit ev: A => CssRhs[CssExpr]): CssRule =
       CssRule(name, rhs.fold(CssRhs.empty)(ev))
 
-    def ?-[A](rhs: js.UndefOr[A])(implicit ev: A => CssRhs[CssExpr]): CssRule =
+    def :-[A](rhs: js.UndefOr[A])(implicit ev: A => CssRhs[CssExpr]): CssRule =
       CssRule(name, rhs.fold(CssRhs.empty)(ev))
   }
 
   final class CssRulePropSyntax private[css] (val name: String) extends AnyVal {
     def :-(prop: CssProp): CssRule = CssRule(name, CssRhs.Value(CssKeyword(prop.propName)))
 
-    def ?-(prop: Option[CssProp]): CssRule =
+    def :-(prop: Option[CssProp]): CssRule =
       CssRule(name, prop.fold(CssRhs.empty)(p => CssRhs.Value(CssKeyword(p.propName))))
 
-    def ?-(prop: js.UndefOr[CssProp]): CssRule =
+    def :-(prop: js.UndefOr[CssProp]): CssRule =
       CssRule(name, prop.fold(CssRhs.empty)(p => CssRhs.Value(CssKeyword(p.propName))))
   }
 }
