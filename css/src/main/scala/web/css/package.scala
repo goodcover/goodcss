b7081f9b9f65ec6dd62cdcba677cc4b6a0a7e0a1
Mickey Donaghy <mickey@goodcover.com>
2023-03-14 08:58:20 +0900

    Revert "Remove the most expensive uses of refined"
    This reverts commit 3b7a790fe3a4d0570d2f504708542f5b5228b0f1.

--- css/src/main/scala/web/css/package.scala
+++ css/src/main/scala/web/css/package.scala
@@ -1,5 +1,7 @@
 package web
 
+import eu.timepit.refined.api.Refined
+import eu.timepit.refined.numeric
 import io.estatico.newtype.macros.newsubtype
 import io.estatico.newtype.ops.toCoercibleIdOps
 
@@ -9,12 +11,9 @@ package object css extends CssFacade with CssImplicits with CssKeywords with Css
 
   type Hue = Double
 
-  // 0-100.0 inclusive
-  type BoundedPercent = Double
-  // 0-1.0 inclusive
-  type BoundedFloat   = Double
-  // 0-255 inclusive
-  type BoundedInt     = Int
+  type BoundedPercent = Double Refined numeric.Interval.Closed[0.0, 100.0]
+  type BoundedFloat   = Double Refined numeric.Interval.Closed[0.0, 1.0]
+  type BoundedInt     = Int Refined numeric.Interval.Closed[0, 255]
 
   @inline implicit def toPrinterOps[A: CssPrinter](target: A): CssPrinter.Ops[A] = new CssPrinter.Ops[A](target)
 
