3b7a790fe3a4d0570d2f504708542f5b5228b0f1
Mickey Donaghy <mickey@goodcover.com>
2023-03-13 14:46:20 +0900

    Remove the most expensive uses of refined

--- css/src/main/scala/web/css/package.scala
+++ css/src/main/scala/web/css/package.scala
@@ -1,7 +1,5 @@
 package web
 
-import eu.timepit.refined.api.Refined
-import eu.timepit.refined.numeric
 import io.estatico.newtype.macros.newsubtype
 import io.estatico.newtype.ops.toCoercibleIdOps
 
@@ -11,9 +9,12 @@ package object css extends CssFacade with CssImplicits with CssKeywords with Css
 
   type Hue = Double
 
-  type BoundedPercent = Double Refined numeric.Interval.Closed[0.0, 100.0]
-  type BoundedFloat   = Double Refined numeric.Interval.Closed[0.0, 1.0]
-  type BoundedInt     = Int Refined numeric.Interval.Closed[0, 255]
+  // 0-100.0 inclusive
+  type BoundedPercent = Double
+  // 0-1.0 inclusive
+  type BoundedFloat   = Double
+  // 0-255 inclusive
+  type BoundedInt     = Int
 
   @inline implicit def toPrinterOps[A: CssPrinter](target: A): CssPrinter.Ops[A] = new CssPrinter.Ops[A](target)
 
