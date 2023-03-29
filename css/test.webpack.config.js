33cc387a9b979920366063898ef289b169b26a4b
Jason Pickens <jasonpickensnz@gmail.com>
2023-03-29 15:38:44 +1300

    Fix cross built client tests

--- css/test.webpack.config.js
+++ css/test.webpack.config.js
@@ -1,6 +1,5 @@
 module.exports = (env) => {
-  const name = "css"
   const baseDir = __dirname
   const common = require('../client-comp/common-test.webpack.config')
-  return common(baseDir, name, env);
+  return common(baseDir, env);
 };
